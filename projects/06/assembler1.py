3fa1719dc0def31e2eab7ccfe759833b0f1f933a
Santiago Larrain <slarrain@uchicago.edu>
2016-02-02 02:07:27 -0600

    damn dictionaries

--- projects/06/assembler1.py
+++ projects/06/assembler1.py
@@ -2,6 +2,20 @@
 
 from rws import remove
 
+djump = {None:"000", "JGT":"001", "JEQ":"010", "JGE":"011", "JLT":"100",
+        "JNE":"101", "JLE":"110", "JMP":"111"}
+ddest = {None:"000", "M":"001", "D":"010", "MD":"011", "A":"100",
+        "AM":"101", "AD":"110", "AMD":"111"}
+dcomp = {"0":"101010","1":"111111","-1":"111010","D":"001100","A":"110000",
+        "!D":"001101","!A":"110001","-D":"001111","-A":"110011",
+        "D+1":"011111","A+1":"110111","D-1":"001110","A-1":"110010",
+        "D+A":"000010","D-A":"010011","A-D":"000111","D&A":"000000",
+        "D|A":"010101","D|M":"010101","D&M":"000000","M-D":"000111",
+        "D-M":"010011","D+M":"000010","M-1":"110010","M+1":"110111",
+        "-M":"110011","!M":"110001","M":"110000"}
+
+
+
 
 def command_type (line):
     '''
@@ -25,12 +39,31 @@ def symbol (line):
         return line[1:-1]
 
 def dest (line):
-
+    if '=' not in line:
+        return None
+    else:
+        return line[:line.index('=')]
 
 def comp ():
+    if '=' in line:
+        return line[line.index('=')+1:]
+    elif ';' in line:
+        return line[:line.index(';')]
+    else:
+        print ("Error with the format of the line: ", line)
+        sys.exit()
 
 def jump ():
+    if ';' not in line:
+        return None
+    else:
+        return line[line.index('=')+1:]
 
+def bin_dest(nm):
+
+def bin_comp(nm):
+
+def bin_jump(nm):
 
 def main(filename):
     fn = filename[:-3]+'out'
