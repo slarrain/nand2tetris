8c82d412217d00642e21edfb1300f5a0abd02f96
Santiago Larrain <slarrain@uchicago.edu>
2016-02-09 03:17:45 -0600

    pointers working

--- projects/07/VMtranslator.py
+++ projects/07/VMtranslator.py
@@ -6,6 +6,8 @@
 #
 
 import glob
+import sys
+from rws import remove
 
 arith = ['add','sub','neg','eq','gt','lt','and','or','not']
 n = 0
@@ -15,11 +17,12 @@ def command_type(line):
     if com in arith:
         return "C_ARITHMETIC"
     else:
-        return "C_"+com.uppercase()
+        return "C_"+com.upper()
 
 def arg1(line):
+    print (line)
     if command_type(line)=="C_RETURN":
-        print "Error. C_RETURN"
+        print ("Error. C_RETURN")
     if command_type(line)=="C_ARITHMETIC":
         return line[0]
     else:
@@ -29,13 +32,15 @@ def arg2(line):
     if command_type(line) in ['C_PUSH','C_POP','C_FUNCTION','C_CALL']:
         return int(line[2])
     else:
-        print "Error. Invalid Call"
+        print ("Error. Invalid Call")
         return
 
 def write_arithmetic (command):
+    global n
+
     instructions = {
-        'add':['@SP', 'MA=M-1', 'D=M', 'A=A+1', 'M=M-D'],
-        'sub': ['@SP', 'MA=M-1', 'D=M', 'A=A-1', 'M=M-D'],
+        'add':['@SP', 'AM=M-1', 'D=M', 'A=A-1', 'M=M+D'],
+        'sub': ['@SP', 'AM=M-1', 'D=M', 'A=A-1', 'M=M-D'],
         'gt':['@SP', 'AM=M-1', 'D=M', 'A=A-1', 'D=M-D', '@FALSE'+str(n),'D;JLE',
                 '@SP', 'A=M-1', 'M=-1', '@CONTINUE'+str(n), '0;JMP',
                 '(FALSE'+str(n)+')','@SP', 'A=M-1', 'M=0', '(CONTINUE'+str(n)+')'],
@@ -46,11 +51,12 @@ def write_arithmetic (command):
                 '@SP', 'A=M-1', 'M=-1', '@CONTINUE'+str(n), '0;JMP',
                 '(FALSE'+str(n)+')','@SP', 'A=M-1', 'M=0', '(CONTINUE'+str(n)+')'],
         'neg': ['@SP', 'A=M-1', 'M=-M'],
-        'and':['@SP', 'MA=M-1', 'D=M', 'A=A+1', 'M=M&D'],
-        'or':['@SP', 'MA=M-1', 'D=M', 'A=A+1', 'M=M|D'],
+        'and':['@SP', 'AM=M-1', 'D=M', 'A=A-1', 'M=M&D'],
+        'or':['@SP', 'AM=M-1', 'D=M', 'A=A-1', 'M=M|D'],
         'not':['@SP','A=M-1','M=!M']
                     }
-    global n+=1
+
+    n+=1
     return instructions[command]
 
 def write_push_pop(command, segment, index, filename):
@@ -70,7 +76,7 @@ def write_push_pop(command, segment, index, filename):
                 v='THIS'
             elif index==1:
                 v='THAT'
-            pre = ['@'+v, 'D=M', '@0', 'A=D+A', 'D=M']
+            pre = ['@'+v, 'D=M']
         elif segment=='static':
             pre = ['@'+filename+str(index), 'D=M']
         return pre+push_end
@@ -81,42 +87,44 @@ def write_push_pop(command, segment, index, filename):
                 v='THIS'
             elif index==1:
                 v='THAT'
-            rv = ['@SP', 'MA=M-1', 'D=M', '@'+v, 'M=D']
+            rv = ['@SP', 'AM=M-1', 'D=M', '@'+v, 'M=D']
         elif segment in segs.keys():
             rv = ['@'+segs[segment], 'D=M', '@'+str(index), 'D=D+A', '@R13', 'M=D',
                     '@SP', 'AM=M-1', 'D=M', '@R13', 'A=M', 'M=D']
         elif segment == 'temp':
-            rv = ['@5', 'D=M', '@'+str(index), 'D=D+A', '@R13', 'M=D',
+            rv = ['@5', 'D=A', '@'+str(index), 'D=D+A', '@R13', 'M=D',
                     '@SP', 'AM=M-1', 'D=M', '@R13', 'A=M', 'M=D']
         elif segment =='static':
-            rv = ['@SP', 'MA=M-1', 'D=M', '@'+filename+str(index), 'M=D']
+            rv = ['@SP', 'AM=M-1', 'D=M', '@'+filename+str(index), 'M=D']
 
         return rv
 
 def main(filenames):
 
-    fo = open (filename[0][:-2]+'asm', 'w')
+    fo = open (filenames[0][:-2]+'asm', 'w')
 
     for filename in filenames:
 
         with open (filename, 'r') as fi:
 
             for line in fi:
+                if line and line[0]!='/' and line!='\n':
+                    line = line.split('\n', 1)[0].split(' ')
 
-                line = line.split(' ')
+                    line_command = command_type(line)
 
-                line_command = command(line)
+                    if command_type(line)!="C_RETURN":
+                        line_arg1 = arg1(line)
+                    if line_command in ['C_PUSH','C_POP','C_FUNCTION','C_CALL']:
+                        line_arg2 = arg2(line)
 
-                if command_type(line)!="C_RETURN":
-                    line_arg1 = arg1(line)
-                if line_command in ['C_PUSH','C_POP','C_FUNCTION','C_CALL']:
-                    line_arg2 = arg2(line)
+                    if line_command=="C_ARITHMETIC":
+                        wl = write_arithmetic(line_arg1)
+                        fo.writelines("%s\n" % l for l in wl)
 
-                if line_command=="C_ARITHMETIC":
-                    fo.writelines(write_arithmetic(line_arg1))
-
-                else:
-                    fo.writelines(write_push_pop(line_command, line_arg1, line_arg2, filename))
+                    elif line_command in ['C_PUSH','C_POP']:
+                        wl = write_push_pop(line_command, line_arg1, line_arg2, filename)
+                        fo.writelines("%s\n" % l for l in wl)
 
     fo.close()
 
